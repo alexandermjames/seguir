c7e364d3ee79d817f2f2b675f94d3977fd060e82
Alexander <alexmtth@amazon.com>
2018-04-19 23:57:18 -0700

    Integration.
    Adding integration with kinesis plugin and contractual driven
    testing.

--- index.js
+++ index.js
@@ -17,45 +17,34 @@ process.once("unhandledRejection", (p, reason) => {
 });
 
 process.once("uncaughtException", (err) => {
-  log.error({
-    reason: err
-  }, "Uncaught exception.");
+  log.error(err, "Uncaught exception.");
 });
 
 const EventEmitter = require("events").EventEmitter;
-const PluginHandler = require("./lib/handlers/plugin-handler.js");
-const FilesHandler = require("./lib/handlers/files-handler.js");
-
-const configHandler = require("./lib/handlers/config-handler.js");
+const Plugins = require("./lib/handlers/plugins.js");
+const Files = require("./lib/handlers/files.js");
+const config = require("./lib/handlers/config.js");
 
 function Seguir(options) {
-  this.pluginHandler = new PluginHandler();
-  this.filesHandler = new FilesHandler();
-}
-
-Seguir.prototype.configure = function(argv) {
-  let context = configHandler.parse(argv);
-  context.emitter = new EventEmitter();
+  let opts = config.parse(options);
+  opts.emitter = new EventEmitter();
+  this.plugins = new Plugins(opts);
+  this.files = new Files(opts);
 
   const destroy = this.destroy.bind(this);
   process.once('SIGINT', destroy);
   process.once('SIGQUIT', destroy);
   process.once('SIGTERM', destroy);
-
-  this.pluginHandler.configure(context);
-  this.filesHandler.configure(context);
 }
 
 Seguir.prototype.destroy = function() {
   log.info("Terminiating seguir instance.");
-
-  this.pluginHandler.destroy();
-  this.filesHandler.destroy();
+  this.plugins.destroy();
+  this.files.destroy();
 }
 
 if (require.main === module) {
-  const seguir = new Seguir();
-  seguir.configure(process.argv);
+  new Seguir(process.argv);
 } else {
   module.exports = Seguir;
 }
